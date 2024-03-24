6724ae16b3fb7f052d71b77fa39bcfe902601392
Matthew <matthewtndirangu@gmail.com>
2024-03-24 23:54:56 +0300

    feat: first submission

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    if (pickupLocation > 42)
+    {return pickupLocation - 42}
+    else if (pickupLocation < 42)
+    return 42 - pickupLocation
+}
+
+distanceFromHqInFeet = function() {
+return distanceFromHqInBlocks * 264
+}
+
